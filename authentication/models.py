384eaaaefa1e28112cd7be0828bfa1fb86df21fb
Chillihache <thomas.leclerc.istria@gmail.com>
2024-12-15 17:02:52 +0100

    Update authentication/models.py
    15/12/2024
    Change self.name to self.first_name and self.last_name

--- authentication/models.py
+++ authentication/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
