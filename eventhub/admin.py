047a0d5272a64bd38a3d71f6d6110c989f521ec4
Aurelien lebray <aurelien.lebray@gmail.com>
2024-12-23 15:27:48 +0100

    Lesson 23122024

--- eventhub/admin.py
+++ eventhub/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from eventhub.models import Client, Contract, Event, User
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserCreationForm, UserChangeForm
+
+class UserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = ('username', 'email', 'employee_number', 'is_staff', 'is_active')
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active', 'employee_number'),
+        }),
+    )
+
 
 
 admin.site.register(Client)
 admin.site.register(Contract)
 admin.site.register(Event)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
--- eventhub/models.py
+++ eventhub/models.py
@@ -5,14 +5,14 @@ import uuid
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("L'email doit être défini")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+    # def create_user(self, email, password=None, **extra_fields):
+    #     if not email:
+    #         raise ValueError("L'email doit être défini")
+    #     email = self.normalize_email(email)
+    #     user = self.model(email=email, **extra_fields)
+    #     user.set_password(password)
+    #     user.save(using=self._db)
+    #     return user
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
